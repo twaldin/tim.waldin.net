e055fefd65a0224ff52bd9af8bea30a9794ab88c
twaldin <timothy@waldin.net>
2026-05-20 23:09:44 -0700

    Update contact X profile link

--- container/scripts/contact.sh
+++ container/scripts/contact.sh
@@ -6,7 +6,7 @@ links_line="${RED}󰇮$(email_link "timothy@waldin.net" "timothy@waldin.net" "$R
 links_line+="${PURPLE}$(hyperlink "github" "https://github.com/twaldin" "$PURPLE")${RESET}    "
 links_line+="${BLUE}$(hyperlink "linkedin" "https://linkedin.com/in/twaldin" "$BLUE")${RESET}    "
 links_line+="${PURPLE}$(hyperlink "instagram" "https://instagram.com/timn.w" "$PURPLE")${RESET}    "
-links_line+="${BLUE}$(hyperlink "twitter" "https://x.com/timhotty1" "$BLUE")${RESET}    "
+links_line+="${BLUE}$(hyperlink "twitter" "https://x.com/twaldin0" "$BLUE")${RESET}    "
 links_line+="${PURPLE}$(hyperlink "website" "https://tim.waldin.net" "$PURPLE")${RESET}    "
 
 echo ""
